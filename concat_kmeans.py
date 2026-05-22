0fb0e8f5b4b2d2d0900c5a08133ab3b1b910af26
60pen9 <111765191+60pen9@users.noreply.github.com>
2026-05-22 17:18:28 +1000

    Update concat_kmeans.py

--- concat_kmeans.py
+++ concat_kmeans.py
@@ -42,9 +42,9 @@ if __name__ == "__main__":
     # dataset = "CIFAR-10"
     # # ["CIFAR-10", "CIFAR-20", "STL-10", "ImageNet-Dogs", "DTD"]
     # tau = 0.01
-    # [0.01, 0.01, 0.01, 0.009, 0.01] ViT-B16
-    # [0.01, 0.012, 0.03, 0.009, 0.01] ViT-L14
-    # [0.007, 0.009, 0.03, 0.009, 0.01] RN50
+    # # [0.01, 0.01, 0.01, 0.009, 0.01] ViT-B16
+    # # [0.01, 0.012, 0.03, 0.009, 0.01] ViT-L14
+    # # [0.007, 0.009, 0.03, 0.009, 0.01] RN50
 
     if dataset == "CIFAR-10" or dataset == "STL-10" or dataset == "ImageNet-10":
         cluster_num = 10
@@ -80,31 +80,10 @@ if __name__ == "__main__":
     )
     labels = np.loadtxt("./data/" + backbone + "/" + dataset + "/" + dataset + "_labels_test.txt")
 
-    # images_embedding = np.load("./data/" + dataset + "_image_embedding_train.npy")
-    # images_embedding = images_embedding / np.linalg.norm(
-    #     images_embedding, axis=1, keepdims=True
-    # )
-    # labels = np.loadtxt("./data/" + dataset + "_labels_train.txt")
-
     nouns_embedding = np.load("./data/" + backbone + "/" + dataset + "/" + dataset + "_filtered_nouns_embedding.npy")
     nouns_embedding = nouns_embedding / np.linalg.norm(
         nouns_embedding, axis=1, keepdims=True
     )
-
-    # tsne = TSNE(n_components=2, random_state=42)
-    # images_embedding_tsne = tsne.fit_transform(images_embedding)
-    # plt.figure(figsize=(6, 6))
-    # plt.scatter(images_embedding_tsne[:, 0], images_embedding_tsne[:, 1], c=labels, cmap='tab20', marker='+', s=5)
-    # # plt.colorbar()  # Show color bar for the target classes
-    # ax = plt.gca()
-    # plt.yticks(size=10)
-    # plt.xticks(size=10)
-    # ax.set_ylim([-100, 100])
-    # ax.set_xlim([-100, 100])
-    # # plt.xlabel("t-SNE component 1")
-    # # plt.ylabel("t-SNE component 2")
-    # # plt.show()
-    # plt.savefig('Figure_1.png', dpi=1000, bbox_inches='tight')
 
     nouns_embedding = torch.from_numpy(nouns_embedding).cuda()
     nouns_num = nouns_embedding.shape[0]
@@ -134,112 +113,7 @@ if __name__ == "__main__":
     concat_embedding = np.concatenate([images_embedding, retrieval_embedding], axis=1)
 
     preds = kmeans(concat_embedding, cluster_num)
-    cluster_metric(labels, preds) #[91.1, 60.6, 98.3, 99.4, 81.2, 50.9, "UCF101", 49.9]
-
-    # tsne = TSNE(n_components=2, random_state=42)
-    # concat_embedding_tsne = tsne.fit_transform(retrieval_embedding)
-    # plt.figure(figsize=(6, 6))
-    # plt.scatter(concat_embedding_tsne[:, 0], concat_embedding_tsne[:, 1], c=labels, cmap='tab20', marker='+', s=3)
-    # ax = plt.gca()
-    # plt.yticks(size=10)
-    # plt.xticks(size=10)
-    # ax.set_ylim([-100, 100])
-    # ax.set_xlim([-100, 100])
-    # # plt.xlabel("t-SNE component 1")
-    # # plt.ylabel("t-SNE component 2")
-    # # plt.show()
-    # plt.savefig('Figure_2.png', dpi=1000, bbox_inches='tight')
-    #
-    # tsne = TSNE(n_components=2, random_state=42)
-    # concat_embedding_tsne = tsne.fit_transform(concat_embedding)
-    # plt.figure(figsize=(6, 6))
-    # plt.scatter(concat_embedding_tsne[:, 0], concat_embedding_tsne[:, 1], c=labels, cmap='tab20', marker='+', s=3)
-    # ax = plt.gca()
-    # plt.yticks(size=10)
-    # plt.xticks(size=10)
-    # ax.set_ylim([-100, 100])
-    # ax.set_xlim([-100, 100])
-    # # plt.xlabel("t-SNE component 1")
-    # # plt.ylabel("t-SNE component 2")
-    # # plt.show()
-    # plt.savefig('Figure_3.png', dpi=1000, bbox_inches='tight')
-
-
-
-
-
-
-
-
-    # images_embedding_train = np.load("./data/" + dataset + "_image_embedding_train.npy")
-    # images_embedding_train = images_embedding_train / np.linalg.norm(
-    #     images_embedding_train, axis=1, keepdims=True
-    # )
-    #
-    # images_embedding_test = np.load("./data/" + dataset + "_image_embedding_test.npy")
-    # images_embedding_test = images_embedding_test / np.linalg.norm(
-    #     images_embedding_test, axis=1, keepdims=True
-    # )
-    # labels = np.loadtxt("./data/" + dataset + "_labels_test.txt")
-    #
-    # nouns_embedding = np.load("./data/" + dataset + "_filtered_nouns_embedding.npy")
-    # nouns_embedding = nouns_embedding / np.linalg.norm(
-    #     nouns_embedding, axis=1, keepdims=True
-    # )
-    #
-    # nouns_embedding = torch.from_numpy(nouns_embedding).cuda()
-    # nouns_num = nouns_embedding.shape[0]
-    #
-    # images_embedding_train = torch.from_numpy(images_embedding_train).cuda()
-    # image_num_train = images_embedding_train.shape[0]
-    #
-    # images_embedding_test = torch.from_numpy(images_embedding_test).cuda()
-    # image_num_test = images_embedding_test.shape[0]
-
-
-    # tau = 0.009 # [0.009, 0.006, 0.01, 0.006, 0.007, 0.008, "UCF101", 0.006]
-    # lamda = 0.009 # [0.009, 0.006, 0.01, 0.006, 0.007, 0.005, "UCF101", 0.006]
-    # batch_size = 8192
-    #
-    # retrieval_similarity = []
-    # for i in range(image_num_train // batch_size + 1):
-    #     start = i * batch_size
-    #     end = start + batch_size
-    #     if end > image_num_train:
-    #         end = image_num_train
-    #         images_batch = images_embedding_train[start:end]
-    #     similarity = torch.matmul(images_embedding_train[start:end], nouns_embedding.T)
-    #     similarity = torch.softmax(similarity / tau, dim=1)
-    #     retrieval_similarity.append(similarity)
-    #     if i % 50 == 0:
-    #         print(f"[Completed {i * batch_size}/{image_num_train}]")
-    #
-    # retrieval_similarity = torch.cat(retrieval_similarity, dim=0).cuda()
-    # retrieval_similarity = torch.mean(retrieval_similarity, dim=0, keepdim=True)
-    #
-    # retrieval_embeddings = []
-    # for i in range(image_num_test // batch_size + 1):
-    #     start = i * batch_size
-    #     end = start + batch_size
-    #     if end > image_num_test:
-    #         end = image_num_test
-    #         images_batch = images_embedding_test[start:end]
-    #     similarity = torch.matmul(images_embedding_test[start:end], nouns_embedding.T)
-    #     similarity = similarity - lamda*torch.log(retrieval_similarity)
-    #     similarity = torch.softmax(similarity / tau, dim=1)
-    #     retrieval_embedding = (similarity @ nouns_embedding).cpu()
-    #     retrieval_embeddings.append(retrieval_embedding)
-    #     if i % 50 == 0:
-    #         print(f"[Completed {i * batch_size}/{image_num_test}]")
-    #
-    # retrieval_embedding = torch.cat(retrieval_embeddings, dim=0).cuda()
-    # retrieval_embedding = F.normalize(retrieval_embedding, dim=1).cpu().numpy()
-    # images_embedding_test = images_embedding_test.cpu().numpy()
-    #
-    # concat_embedding = np.concatenate([images_embedding_test, retrieval_embedding], axis=1)
-    # preds = kmeans(concat_embedding, cluster_num)
-    # cluster_metric(labels, preds) #[91.1, 60.6, 98.3, 99.4, 81.2, 50.9, "UCF101", 49.9]
-
+    cluster_metric(labels, preds) 
 
 
 
